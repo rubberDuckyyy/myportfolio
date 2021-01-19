f611e306903e427bc6aa9e67ada71418e371f336
Tony Kim <cs.tkim@gmail.com>
2021-01-18 23:59:10 -0800

    updated

--- src/js/sr.js
+++ src/js/sr.js
@@ -3,7 +3,7 @@ export default function() {
   const defaultProps = {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     distance: '30px',
-    duration: 1000,
+    duration: 800,
     desktop: true,
     mobile: true
   };
@@ -29,7 +29,7 @@ export default function() {
   ScrollReveal().reveal('.hero-cta', 
     assignProps(
       {
-        delay: 1000, 
+        delay: 700, 
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -38,7 +38,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__image', 
     assignProps(
       {
-        delay: 600,
+        delay: 500,
         origin: 'bottom'
       }, defaultProps)
   );
@@ -46,7 +46,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__info', 
     assignProps(
       {
-        delay: 1000,
+        delay: 700,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -55,7 +55,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__text', 
     assignProps(
       {
-        delay: 500,
+        delay: 400,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -63,7 +63,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__image', 
     assignProps(
       {
-        delay: 1000,
+        delay: 600,
         origin: window.innerWidth > 768 ? 'right' : 'bottom'
       }, defaultProps)
   );
@@ -72,7 +72,7 @@ export default function() {
   ScrollReveal().reveal('.contact-wrapper', 
     assignProps(
       {
-        delay: 800,
+        delay: 700,
         origin: 'bottom'
       }, defaultProps)
   );
